e89528fb831171c3969f66d5fc486142d763ad89
Ленар Хабибуллин <leonruscraft@gmail.com>
2022-06-14 13:29:07 +0500

    Add project files.

---
+++ PatternStrategy/JRPG.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternStrategy
+{
+    class JRPG : IRPG
+    {
+        public void SelectGame()
+        {
+            Console.WriteLine("JRPG is selected!");
+        }
+    }
+}
